b8483972bc5e99265fb0ea0a1c6dea44d250477a
Trippie <antoninhede277@gmail.com>
2024-03-05 03:05:47 +0100

    Ajout de la fonction ft_atoi

---
+++ ft_atoi.py
@@ -0,0 +1,19 @@
+def ft_atoi(str):
+
+    result = 0
+    i = 0
+    sign = 1
+    while str[i:] and (str[i] == ' ' or str[i] == '\t' or str[i] == '\n' or str[i] == '\v' or str[i] == '\f'):
+        i += 1
+    if str[i:] and (str[i] == '-' or str[i] == '+'):
+        sign = -1 if str[i] == '-' else 1
+        i += 1
+    while str[i:] and '0' <= str[i] <= '9':
+        result = result * 10 + int(str[i])
+        i += 1
+    return sign * result
+
+
+nb = 12
+nb2 = nb + ft_atoi(" 1")
+print(nb2)
