92608fcedcb0cbbbc1d737cd8a9b42da97e0e277
Trippie <antoninhede277@gmail.com>
2024-03-05 01:23:03 +0100

    Bibliotheque python

---
+++ biblio.py
@@ -0,0 +1,17 @@
+def addition(*nombres):
+    somme = 0
+    for nombre in nombres:
+        somme += nombre
+    return somme
+
+
+def subtraction(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+
+
